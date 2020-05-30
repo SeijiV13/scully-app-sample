8790d6ef455ffa82384a89779f07a44e56669c20
Seiji Ralph Villafranca <seijiv13@outlook.com>
2020-05-30 14:31:33 +0800

    added initial components for demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
+
+import { PageTwoComponent } from './page-two/page-two.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageOneComponent } from './page-one/page-one.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'one',
+    component: PageOneComponent
+  },
+  {
+    path: 'two',
+    component: PageTwoComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled',
+    useHash: true
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { PageOneComponent } from './page-one/page-one.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+import { PageTwoComponent } from './page-two/page-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PageOneComponent, PageTwoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
